8b295fb04b2252de3100a9efceb5ce72133cac9f
ETTSU-StarTail <holywolf53@gmail.com>
2023-03-03 04:15:43 +0900

    一旦もうべたで。

--- src/routes/api/cf/env/+server.ts
+++ src/routes/api/cf/env/+server.ts
@@ -4,7 +4,10 @@ import { NEWT_SPACE_UID, NEWT_CDN_API_TOKEN } from '$env/static/private';
 
 export const GET = (async ({ platform }) => {
 	return json({
-		spaceUid: import.meta.env.NEWT_SPACE_UID ?? NEWT_SPACE_UID,
-		token: import.meta.env.NEWT_CDN_API_TOKEN ?? NEWT_CDN_API_TOKEN
+		spaceUid: platform?.env?.NEWT_SPACE_UID ?? NEWT_SPACE_UID ?? 'ettsu',
+		token:
+			platform?.env?.NEWT_CDN_API_TOKEN ??
+			NEWT_CDN_API_TOKEN ??
+			'jEQ-aE4V5HlQXEJqVr7nmj0hBFE_JcYb8lBGOiEABDQ'
 	});
 }) satisfies RequestHandler;
